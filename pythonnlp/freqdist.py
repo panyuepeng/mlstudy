e61f0ce28a636fbf4d14855e942535e13c764e56
PanYuepeng <panyuepeng@jd.com>
2018-06-15 20:03:27 +0800

    sdfs

--- pythonnlp/freqdist.py
+++ pythonnlp/freqdist.py
@@ -2,10 +2,26 @@
 # Author: pan
 # Filename: 
 
+"""
+
+"""
 
 
 import numpy as np
 import scipy
+import nltk
+from nltk.corpus import brown
+cfd = nltk.ConditionalFreqDist((genre, word) for genre in brown.categories() for word in brown.words(categories=genre))
+# cfd.plot()
+
+
+genre_word = [(genre,word) for genre in ['news','romance']
+              for word in brown.words(categories=genre)]
+print len(genre_word)
+
+
+
+
 
 if __name__ == '__main__':
     pass
