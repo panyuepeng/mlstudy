7e934b2a091bbb1f460f63f16924ec8592e2be1b
PanYuepeng <panyuepeng@jd.com>
2018-07-11 10:46:04 +0800

    sgdfg

--- collectiveintelligenceprogramming/study/ch5/optimization.py
+++ collectiveintelligenceprogramming/study/ch5/optimization.py
@@ -38,14 +38,29 @@ def getminutes(t):
     :return:
     """
     x = time.strptime(t, '%H:%M')
+    print x
+    #
     return x[3]*60+x[4]
 
 
 def printschedule(r):
+    """
+    格式化输出
+    :param r:
+    :return:
+    """
     for d in range(len(r) / 2):
         name = people[d][0]
         origin = people[d][1]
+        out = flights[(origin, destination)][int(r[d])]
+        ret = flights[(destination, origin)][int(r[d + 1])]
+        print '%10s%10s %5s-%5s $%3s %5s-%5s $%3s' % (name, origin,
+                                                      out[0], out[1], out[2],
+                                                      ret[0], ret[1], ret[2])
 
 
 if __name__ == '__main__':
+
+    getminutes("13:24")
+
     pass
